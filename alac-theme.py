f759bc1bd6a578dd7d146145c488b9f4772a987b
ritik <58474947+ahmRitik@users.noreply.github.com>
2021-04-08 06:36:47 +0530

    added -d

--- alac-theme.py
+++ alac-theme.py
@@ -1,12 +1,12 @@
 import argparse
 
-from alac import list_themes , current_theme , changing_theme
+from alac import list_themes , current_theme , changing_theme , writing_default_config
 
 parser = argparse.ArgumentParser(description="Simple Python Script for changing alacritty terminal emulator theme")
 parser.add_argument("-l" , "--list" , help="Listing out all available themes" , action="store_true")
 parser.add_argument("-t" , "--theme" , help="Set theme by passing theme name")
 parser.add_argument("-c" , "--curr" , help="Shows current theme of alacritty" , action="store_true")
-
+parser.add_argument("-d" , "--default" , help="Write my alacritty config" , action="store_true")
 args = parser.parse_args()
 
 if args.list:
@@ -20,3 +20,6 @@ if args.theme:
 
 if args.theme:
     changing_theme(args.theme)
+
+if args.default:
+    writing_default_config()
